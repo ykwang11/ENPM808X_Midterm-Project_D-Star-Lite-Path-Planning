e8e79062e3024a1d52ba63224c319b9f1f1f8f23
Yu-Kai Wang <ykwang11@terpmail.umd.edu>
2018-10-12 22:50:45 -0700

    fix bugs in CellTest

--- test/CellTest.cpp
+++ test/CellTest.cpp
@@ -5,10 +5,10 @@
 
 TEST(CellTest, testCell) {
   Cell cell_test;
-  EXPECT_EQ(cell_test.currentG(), 10000);
-  EXPECT_EQ(cell_test.currentRhs(), 10000);
-  cell_test.updateG(10);
-  cell_test.updateRhs(10);
-  EXPECT_EQ(cell_test.currentG(), 10);
-  EXPECT_EQ(cell_test.currentRhs(), 10);
+  EXPECT_EQ(cell_test.CurrentG(), 10000);
+  EXPECT_EQ(cell_test.CurrentRhs(), 10000);
+  cell_test.UpdateG(10);
+  cell_test.UpdateRhs(10);
+  EXPECT_EQ(cell_test.CurrentG(), 10);
+  EXPECT_EQ(cell_test.CurrentRhs(), 10);
 }
